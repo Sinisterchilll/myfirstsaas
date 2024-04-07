4e5777bd783c7facc064fcc9b6ada919be182833
Sinisterchilll <anucbs22@gmail.com>
2024-04-07 23:11:31 +0530

    added auth-callback

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
+
+const Page = () => {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const origin = searchParams.get('origin')
+  
+    const { data, error, isLoading } = trpc.authCallback.useQuery(undefined, {
+      retry: true,
+      retryDelay: 500,
+    })
+  
+    if (isLoading) {
+      return (
+        <div className='w-full mt-24 flex justify-center'>
+          <div className='flex flex-col items-center gap-2'>
+            <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+            <h3 className='font-semibold text-xl'>
+              Setting up your account...
+            </h3>
+            <p>You will be redirected automatically.</p>
+          </div>
+        </div>
+      )
+    }
+  
+    if (error) {
+      if (error.data?.code === 'UNAUTHORIZED') {
+        router.push('/sign-in')
+      } else {
+        console.error('Error:', error)
+        // Handle other error cases as needed
+      }
+      return null
+    }
+  
+    // Check for success
+    if (data?.success) {
+      // User is synced to db
+      router.push(origin ? `/${origin}` : '/dashboard')
+      return null
+    }
+  
+    // If none of the above conditions are met, return null or an appropriate component
+    return null
+  }
+  
+  export default Page
+  
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
-const page = () => {
-    return (
-        <></>
-    )
+ import { db } from '@/db'
+// import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
+import Dashboard from '../components/Dashboard'
+
+const Page = async () => {
+  const {getUser} = getKindeServerSession()
+  const user = await getUser()
+    
+  if (!user || !user.email) redirect('/auth-callback?origin=dashboard')
+
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
+
+  if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
+//   const subscriptionPlan = await getUserSubscriptionPlan()
+
+//   return <Dashboard subscriptionPlan={subscriptionPlan} />
+return <Dashboard />
 }
 
-export default page
+export default Page
